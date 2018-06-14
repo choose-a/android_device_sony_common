b31e075e140af591dd5e47da419970dc88c95646
Erik Castricum <git@cas-online.nl>
2018-06-14 15:53:53 +0200

    sony:common: add dualsim properties
    Change-Id: I03627446f7de21bf20a70099c499eb09690d7d09
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- init/init_variant.cpp
+++ init/init_variant.cpp
@@ -74,8 +74,7 @@ void vendor_load_properties()
     __system_property_get("ro.sony.variant", model);
     __system_property_get("ro.choose-a.device", codename);
     // Set Properties
-    property_set("ro.product.model", model);
-    property_set("ro.product.device", codename);
+    property_override("ro.product.device", codename);
 
 #if VARIANT_GSM
     for (int i = 0; i < (signed)(sizeof(variants)/(sizeof(variants[0]))); i++) {
@@ -87,7 +86,11 @@ void vendor_load_properties()
 
     if (variantID >= 0) {
         if (variants[variantID].is_ds) {
+            property_set("persist.multisim.config", "dsds");
             property_set("persist.radio.multisim.config", "dsds");
+            property_set("ro.telephony.ril.config", "simactivation");
+            property_override("ro.telephony.default_network", "9,1");
+            property_override("ro.product.model", model);
         }
     }
 #endif
